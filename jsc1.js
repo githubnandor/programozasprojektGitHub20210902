bc8b85bbb2e6dba11ab2dfb9f099177685e89242
githubnandor <nandor.raczvalach@gmail.com>
2021-09-09 12:06:48 +0200

    feltöltöttem a stage-re

---
+++ jsc1.js
@@ -0,0 +1,9 @@
+var a = 12
+var b = 5
+function Teszt() {
+    document.writeln(a + b+"<br>");
+    document.writeln(b + a+"<br>");
+    document.writeln(a-b+"<br>");
+    document.writeln(a*b+"<br>");
+    document.writeln(a/b+"<br>");
+}
